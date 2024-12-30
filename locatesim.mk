33d1d783c679cc7db2793c3d427c5c13ace1270a
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2024-12-30 12:03:16 +0700

    iOS 16.0 and up are read-only

--- locatesim.mk
+++ locatesim.mk
@@ -1,6 +1,6 @@
 PL_SIMULATOR_VERSION := 15.0
 
-ifeq ($(shell echo "$(PL_SIMULATOR_VERSION) >= 17.0" | bc),1)
+ifeq ($(shell echo "$(PL_SIMULATOR_VERSION) >= 16.0" | bc),1)
 PL_SIMULATOR_ROOT = /Library/Developer/CoreSimulator/Volumes/*/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ $(PL_SIMULATOR_VERSION).simruntime/Contents/Resources/RuntimeRoot
 else
 PL_SIMULATOR_ROOT = /Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ $(PL_SIMULATOR_VERSION).simruntime/Contents/Resources/RuntimeRoot
