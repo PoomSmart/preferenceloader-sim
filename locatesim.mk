53217893bcac3abd1c1720d7527429b09f751ecc
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2025-01-25 21:17:52 +0700

    Do not escape

--- locatesim.mk
+++ locatesim.mk
@@ -6,6 +6,6 @@ else
 PL_SIMULATOR_ROOT = /Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ $(PL_SIMULATOR_VERSION).simruntime/Contents/Resources/RuntimeRoot
 endif
 
-PL_SIMULATOR_APPLICATION_SUPPORT_PATH = $(PL_SIMULATOR_ROOT)/Library/Application\ Support
+PL_SIMULATOR_APPLICATION_SUPPORT_PATH = $(PL_SIMULATOR_ROOT)/Library/Application Support
 PL_SIMULATOR_BUNDLES_PATH = $(PL_SIMULATOR_ROOT)/Library/PreferenceBundles
 PL_SIMULATOR_PLISTS_PATH = $(PL_SIMULATOR_ROOT)/Library/PreferenceLoader/Preferences
