c21d9e21d3404b3b1f827ac3cc2693e92928eed8
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2025-04-02 19:56:04 +0700

    Handle whitespace correctly

--- locatesim.mk
+++ locatesim.mk
@@ -3,9 +3,9 @@ PL_SIMULATOR_VERSION := 15.0
 ifeq ($(shell echo "$(PL_SIMULATOR_VERSION) >= 16.0" | bc),1)
 PL_SIMULATOR_ROOT = $(shell echo /Library/Developer/CoreSimulator/Volumes/*/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ $(PL_SIMULATOR_VERSION).simruntime/Contents/Resources/RuntimeRoot)
 else
-PL_SIMULATOR_ROOT = /Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ $(PL_SIMULATOR_VERSION).simruntime/Contents/Resources/RuntimeRoot
+PL_SIMULATOR_ROOT = $(shell echo "/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS $(PL_SIMULATOR_VERSION).simruntime/Contents/Resources/RuntimeRoot")
 endif
 
-PL_SIMULATOR_APPLICATION_SUPPORT_PATH = $(PL_SIMULATOR_ROOT)/Library/Application Support
+PL_SIMULATOR_APPLICATION_SUPPORT_PATH = $(shell echo "$(PL_SIMULATOR_ROOT)/Library/Application Support")
 PL_SIMULATOR_BUNDLES_PATH = $(PL_SIMULATOR_ROOT)/Library/PreferenceBundles
 PL_SIMULATOR_PLISTS_PATH = $(PL_SIMULATOR_ROOT)/Library/PreferenceLoader/Preferences
